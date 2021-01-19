c080012170a0e80bff5aa74f1ab2b93d19d5f60f
DarshanASC6 <darshnyc@gmail.com>
2021-01-19 10:44:19 -0500

    Finished the base script. I may add in a .json file later on for increased customizability and versatility, but for now it works fine.

--- save.py
+++ save.py
@@ -1,11 +1,21 @@
 import os
 import signal
-import time 
+import time
+import pyautogui
 from subprocess import check_output
 
 # Get all of the PID's of a running process
 def get_pid(name):
-    return list(map(int,check_output(["pidof",name]).split()))
+    # Save all of the PID's into a variable, so I can verify that it is not null
+    pid_list = list(map(int,check_output(["pidof",name]).split()))
+
+    # Check if pid_list is null or not
+    if len(pid_list) == 0:
+        print('Process' + name + 'does not seem to be running')
+        quit()
+
+    else:
+        return pid_list
 
 # Kill the process based on all of the process ID's. Use SIGTERM to cleanly exit the processes.
 def kill_pid(pid):
@@ -14,17 +24,17 @@ def kill_pid(pid):
         os.kill(pid[i], signal.SIGTERM)
 
 # Include some way of sending out a key press. I'd prefer to do this with a default library, but if I really need to I'm cool having a library auto-install itself.
-# def key_input():
-#    try:
-#        import 
+def key_input():
+    pyautogui.press('f5')
 
-def countdown(time):
-    while time:
-        # Convert time form minutes into seconds (60 seconds in 1 minute)
-        time *= 60
+def countdown(t):
 
+    # Convert time form minutes into seconds (60 seconds in 1 minute)
+    t *= 60
+
+    while t:
         # Break up time into minutes and seconds (to be displayed later)
-        mins, secs = divmod(time, 60)
+        mins, secs = divmod(t, 60)
 
         # Format time into minutes and seconds
         timer = '{:02d}:{:02d}'.format(mins, secs)
@@ -32,8 +42,10 @@ def countdown(time):
         # Print out minutes and seconds in "minutes:seconds" format
         print(timer, end="\r")
 
+        time.sleep(1)
+
         # Count down as time goes on
-        time -= 1
+        t -= 1
 
     print("Time's up!")
 
@@ -41,8 +53,11 @@ def countdown(time):
 
 # Execution:
 
+# Name of the PID (to be used later
+pid = "steam"
+
 # Save the output of get_pid() to a variable, to be used elsewhere
-global_pid = get_pid("steam")
+global_pid = get_pid(pid)
 
 # Be sure that we have some output for the pid.
 print(global_pid)
@@ -53,5 +68,12 @@ time_limit = input("Enter the time limit for the play session (in minutes): ")
 # Start the countdown timer with the time limit variable
 countdown(int(time_limit))
 
-# After the timer is done, kill the process
+# Press the 'save' key (NOTE: f5 is the default save key for Fallout: New Vegas. In order to use this script for other games, this will have to be changed or commented out.)
+# key_input()
+
+# Wait 1 minute for the game to properly save the game, so as not to corrupt a save (NOTE: From my experimentation, this is fine for my config. However, New Vegas seems to be held together by spit and frayed duct tape at time of writing on other OS's, from what I understand. As such, this approach of saving the game, waiting a bit, and then killing it outright rather than properly waiting may not work for some.)
+print('Waiting a bit to properly save game')
+countdown(int(1))
+
+# After the timer is done, and the game has (hopefully) properly saved itself, kill the entire game based on the PID.
 kill_pid(global_pid)
